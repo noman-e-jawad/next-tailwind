dc4c2be625b512614819d3c03b06c3c9f5aa5ba1
Noman-zawad <67721965+Noman-zawad@users.noreply.github.com>
2022-07-04 18:16:04 +0600

    next-tailwind kickoff ⚽🦶🦵

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        primary: ["Be Vietnam Pro", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          red: "#f25f3a",
+          blue: "#242d52",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
