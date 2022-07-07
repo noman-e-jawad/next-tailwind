947d7db74dcca084bb2aa3bdabb2747ae6f8a6f3
Noman-zawad <67721965+Noman-zawad@users.noreply.github.com>
2022-07-07 15:30:56 +0600

    Added global style sheet

---
+++ src/components/Features.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <section>
+      <div className="">
+        <div className="">
+          <div className="">
+            <h2> What’s different about Manage?</h2>
+            <p>
+              Manage provides all the functionality your team needs, without the
+              complexity. Our software is tailor-made for modern digital product
+              teams.
+            </p>
+          </div>
+          <div className="">
+            <div className="">
+              <h3>
+                <span>01</span>Track company-wide progress
+              </h3>
+              <p>
+                See how your day-to-day tasks fit into the wider vision. Go from
+                tracking progress at the milestone level all the way done to the
+                smallest of details. Never lose sight of the bigger picture
+                again.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
