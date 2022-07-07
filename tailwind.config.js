5b7c0d474d8d1d53e5737f10cdb49b913d789868
Noman-zawad <67721965+Noman-zawad@users.noreply.github.com>
2022-07-07 18:29:22 +0600

    updated global stylesheet and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,16 @@ module.exports = {
       fontFamily: {
         primary: ["Be Vietnam Pro", "sans-serif"],
       },
+      fontSize: {
+        "2.5xl": "28px",
+      },
       colors: {
         primary: {
           red: "#f25f3a",
           blue: "#242d52",
+        },
+        light: {
+          orange: "#FFF0EB",
         },
       },
     },
