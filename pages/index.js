dbf7274ffb693dbf52ed2498ad0625f61465f548
Noman-zawad <67721965+Noman-zawad@users.noreply.github.com>
2022-07-23 12:23:02 +0600

    cta completed

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import Features from "../src/components/Features";
 import Testimonial from "../src/components/Testimonial";
+import CTA from "../src/components/CTA";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <Features></Features>
+      <Features />
       <Testimonial />
+      <CTA />
     </div>
   );
 }
--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return <section className="container-fluid bg-primary-red h-96 flex flex-col justify-center items-center px-10 gap-5 mt-5 md:flex-row md:justify-between md:px-40">
+    <h2 className="banner-heading text-white md:w-1/2 md:text-left">Simplify how your team works today.</h2>
+    <button className="button-primary bg-white text-primary-red">Get Started</button>
+  </section>;
 };
 
 export default CTA;
