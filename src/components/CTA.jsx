ddc5d56691934ac6306ee2cebbce8affb654949f
Noman-zawad <67721965+Noman-zawad@users.noreply.github.com>
2022-07-23 12:07:33 +0600

    cta

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 const CTA = () => {
-  return (
-    <section>
-      
-    </section>
-  );
+  return <section className="container-fluid h-96 bg-primary-red flex flex-col items-center justify-center gap-6 px-12 mt-11 md:flex-row md:justify-between lg:px-40">
+    <h2 className="banner-heading text-white md:text-left lg:w-1/2">Simplify how your team works today.</h2>
+    <button className="button-primary bg-white text-primary-red"> Get Started</button>
+  </section>;
 };
 
 export default CTA;
