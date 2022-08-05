ae1a99344a7358604512545d418478881f3387d3
Noman-zawad <nomanzawad@gmail.com>
2022-08-05 17:29:39 +0600

    feature section -- backgound, cta section -- background, footer -- alignment fix, header, banner, testimonial -- no change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,41 +1,82 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className="fluid bg-dark-blue py-12 flex flex-col items-center justify-center gap-12 w-full md:flex-row md:items-start relative">
-    {/* footer form */}
-    <div className="flex flex-row justify-between w-full order-1 md:order-4">
-      <input type="email" placeholder="Updates in your inbox" className="button-primary bg-white body-text focus-visible:outline-0 text-left font-normal shadow-none" />
-      <button type="submit" className="button-primary shadow-none">GO</button>
-    </div>
-    {/* footer navigation */}
-    <div className="flex flex-row justify-between w-full px-9 order-2">
-        <ul className="nav-item">
-          <li>Home</li>
-          <li>Pricing</li>
-          <li>Products</li>
-          <li>About Us</li>
+  return (
+    <footer className=" fluid w-full  h-full flex flex-col justfiy-center gap-12 items-center bg-dark-blue py-12 md:flex-row-reverse md:items-start  ">
+      {/* Form */}
+      <div className="flex gap-4">
+        <input
+          type="email"
+          placeholder="Updates on your inbox"
+          className="text-input"
+        />
+        <input
+          type="submit"
+          value="Submit"
+          className="button-primary shadow-none"
+        />
+      </div>
+      {/* Menu */}
+      <div className="flex w-full justify-center gap-24 text-white">
+        <ul className="flex flex-col gap-2">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Products</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
         </ul>
-        <ul className="nav-item">
-          <li>Careers</li>
-          <li>Community</li>
-          <li>Privacy Policy</li>
+        <ul className="flex flex-col gap-2">
+          <li>
+            <a href="#">Carrers</a>
+          </li>
+          <li>
+            <a href="#">Community</a>
+          </li>
+          <li>
+            <a href="#">Privacy Policy</a>
+          </li>
         </ul>
-    </div>
-    {/* icon box */}
-    <div className="flex flex-col gap-12 w-full items-center order-3 md:order-1 md:flex-col-reverse md:justify-between h-36">
-      <ul className="flex flex-row gap-9 md:gap-3">
-        <li><i ><img className="social-icon" src="/images/icon-facebook.svg" /></i></li>
-        <li><i ><img className="social-icon" src="/images/icon-youtube.svg" /></i></li>
-        <li><i ><img className="social-icon" src="/images/icon-twitter.svg" /></i></li>
-        <li><i ><img className="social-icon" src="/images/icon-pinterest.svg" /></i></li>
-        <li><i ><img className="social-icon" src="/images/icon-instagram.svg" /></i></li>
-      </ul>
-    {/* footer logo */}
-    <img className="w-40 h-auto order-2" src="/images/logo-white.svg" />
-    </div>
-    {/* copyright */}
-    <p className="text-dark-graishBlue text-sm order-4 md:order-3 md:absolute md:self-end md:right-40">Copyright 2020. All Rights Reserved</p>
-  </footer>
+      </div>
+      {/* Extras */}
+      <div className="flex w-full flex-col gap-8 md:gap-0 justify-between min-h-full items-center md:flex-col-reverse md:h-36 md:justify-between md:items-center">
+        <div className="flex gap-6">
+          {/* Social Icons */}
+          <img
+            src="/images/icon-facebook.svg"
+            className="w-8 h-8 object-contain"
+          />
+          <img
+            src="/images/icon-instagram.svg"
+            className="w-8 h-8 object-contain"
+          />
+          <img
+            src="/images/icon-pinterest.svg"
+            className="w-8 h-8 object-contain"
+          />
+          <img
+            src="/images/icon-twitter.svg"
+            className="w-8 h-8 object-contain"
+          />
+          <img
+            src="/images/icon-youtube.svg"
+            className="w-8 h-8 object-contain"
+          />
+        </div>
+        <img src="/images/logo-white.svg" alt="" className="w-64" />
+      </div>
+
+      <span className="text-white md:absolute md:bottom-0 md:pb-12">
+        Copyright 2022. All Rights Reserved
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
